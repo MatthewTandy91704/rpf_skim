706f8cce73ed821291029a3c0db829cb4ca1c958
MatthewTandy91704 <mtandy@purdue.edu>
2025-11-23 05:48:27 -0500

    work learning the fundamentals of spacy library

---
+++ scaCy_test.py
@@ -0,0 +1,12 @@
+import spacy
+
+nlp = spacy.blank("en")
+
+doc = nlp("It costs $5.")
+
+print("Index: ", [token.i for token in doc])
+print("Text: ", [token.text for token in doc])
+
+print("is_alpha:", [token.is_alpha for token in doc])
+print("is_punct:", [token.is_punct for token in doc])
+print("like_num:", [token.like_num for token in doc])
