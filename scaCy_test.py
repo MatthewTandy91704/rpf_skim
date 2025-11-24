b4ad7a1a482463fd4953335a69b800b33848d561
MatthewTandy91704 <mtandy@purdue.edu>
2025-11-24 00:50:51 -0500

    extra tester

--- scaCy_test.py
+++ scaCy_test.py
@@ -6,11 +6,21 @@ nlp = spacy.load("en_core_web_sm")
 
 matcher = Matcher(nlp.vocab)
 
-pattern = [{"TEXT": "iPhone"}, {"TEXT": "X"}]
-matcher.add("IPHONE_PATTERN", [pattern])
+pattern = [{"IS_DIGIT": True},
+           {"LOWER": "fifa"},
+           {"LOWER": "world"},
+           {"LOWER": "cup"},
+           {"IS_PUNCT": True}
+           ]
 
-doc = nlp("Upcoming iPhone X release date leaked")
+doc = nlp("2018 FIFA World Cup: France won!")
 
-matches = matcher(doc)
+pattern = [
+    {"LEMMA": "love"}, {"POS": "VERB"}, {"POS": "NOUN"}
+]
 
-print(matches)
+doc = nlp("I loved dogs but not I love cats more.")
+
+pattern = [{"LEMMA": "buy"}, {"POS": "DET", "OP": "?"}, {"POS": "NOUN"}]
+
+doc = nlp("I bought a smartphone. Now I'm buying apps.")
