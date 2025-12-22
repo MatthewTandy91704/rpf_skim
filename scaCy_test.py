fd99abb57444149cfdc2902079264adf2019f972
MatthewTandy91704 <mtandy@purdue.edu>
2025-12-22 01:41:23 -0500

    proof of concept for distance finding

--- scaCy_test.py
+++ scaCy_test.py
@@ -43,7 +43,9 @@ matcher.add("IOS_VERSION_PATTERN", [pattern])
 matches = matcher(doc)
 print("Total matches found:", len(matches))
 
-# testing for actual use case
+# testing for actual use case using an old message from Sol as my nlp
+# this leads to a full proof of concept for at least the factor of picking out distancing
+# in fact distancing would be very easy
 
 test = nlp("Umeko’s expression shifted in confusion underneath her mask as she caught her balance and adjusted to the new environment. She disliked the small fighting space, much preferring open spaces, but oh well, she can manage. The barrier was impressive as well, in her mind, as someone who hasn’t had much experience with them at all. She skipped backwards to create 10 meters between them total, being careful to avoid the various pockets of lava as her hands flowed through signs swiftly. It just so happened that she needed practice with water release anyways. An orb of water formed in front of her, from which 7 sharp whips shot out at Dixue from the orb, appearing to aim straight for her abdomen, but 4 of the whips suddenly shifted its trajectory and bent in odd ways that led to one aiming for her left leg, one to her right arm, one to her neck, and the last seemingly going up and past her head. If she wasn’t careful, that last whip would bend again and snap at her back. They were fully capable of cutting through her armor, and deep into her skin due to the strength of this particular technique and Umeko’s control over her chakra allowing her to pinpoint those locations with dangerous sharpness")
 
