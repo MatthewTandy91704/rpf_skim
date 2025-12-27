fd5449656fe5a29bde9fef03a518481af41cfbb8
MatthewTandy91704 <mtandy@purdue.edu>
2025-12-26 21:39:16 -0500

    work on custom pipeline elements

--- Tester Files/scaCy_test_4.py
+++ Tester Files/scaCy_test_4.py
@@ -14,9 +14,39 @@ import spacy
 
 from spacy.matcher import Matcher
 
-nlp = spacy.blank("en")
+nlp = spacy.load("en_core_web_md")
 
 doc = nlp("This is a sentence.")
 
 print(nlp.pipe_names)
 print(nlp.pipeline)
+
+# custom pipeline components can be added to auto call a funct when nlp is called
+# add own metadata to docs and tokens
+
+from spacy.language import Language
+
+@Language.component("custom_component")
+
+def custom_component_function(doc):
+    # do something to the doc here
+    return doc
+
+nlp.add_pipe("custom_component")
+
+nlp = spacy.load("en_core_web_sm")
+
+@Language.component("custom")
+def custom_function(doc):
+    print("Doc length:", len(doc))
+    return doc
+
+nlp.add_pipe("custom", first=True)
+
+print("Pipeline:", nlp.pipe_names)
+
+doc = nlp("Hello world")
+
+# we can solve the problem of computing values based on tokens and their attributes
+# as well as adding named entities, for example based on a dictionary
+# by adding custom pipeline components
